448df41aaf5e687faf1fa92a595e2343dbc40f32
Rodrigo Rauber <rodriigorauber@gmail.com>
2023-09-23 22:16:33 -0300

    first commit

---
+++ src/Contador.java
@@ -0,0 +1,39 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+        
+    
+        try {
+			//chamando o método contendo a lógica de contagem
+			contar(parametroUm, parametroDois);
+		
+		}catch (ParametrosInvalidosException  exception) {
+            System.out.println(exception);
+			System.out.println("O segundo parâmetro deve ser maior que o primeiro.");
+		}
+
+    }   
+    
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+        int valor = 1;
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        }else{
+
+            for (int i = parametroUm; i < parametroDois; i++) {
+                System.out.println("Imprimindo o número " + valor);
+                valor++;
+            }
+
+        }
+
+    } 
+}
